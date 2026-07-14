b5b12ac69ddfb2181016511ea729408fa1b24af4
Dhruvsheth07 <drsheth10@gmail.com>
2026-07-14 23:56:55 +0530

    feat: implement recursive array reversal function

---
+++ recursion/reverseArr.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void revArr(vector<int>& arr,int s,int f){
+    if(s>=f){return;}
+    swap(arr[s],arr[f]);
+    revArr(arr,s+1,f-1);
+}
+int main(){
+    vector<int> arr={1,2,3,5,5};
+    int s=0,f=arr.size()-1;
+    // while(s!=f){
+    //     swap(arr[s],arr[f]);
+    //     s++;
+    //     f--;
+    // }
+    revArr(arr,s,f);
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}       
